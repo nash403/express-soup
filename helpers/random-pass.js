175dc6854fe0165d2b664b5ae827d1147efbf1e2
nash403 <nash403@outlook.fr>
2017-02-15 22:23:14 +0100

    feat() add random password generator

---
+++ helpers/random-pass.js
@@ -0,0 +1,10 @@
+function newPassword(length = 10) {
+  let newPass = ''
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@!?=+-_;: $€#*'
+  for (let i = 0; i < length; i++) {
+    newPass += characters.charAt(Math.floor(Math.random() * characters.length))
+  }
+  return newPass
+}
+
+module.exports = newPassword
