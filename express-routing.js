8b43d4f16764da3d4888662ba9f001dd17693680
nash403 <nash403@outlook.fr>
2017-02-18 18:59:50 +0100

    remove debug log

--- express-routing.js
+++ express-routing.js
@@ -3,7 +3,6 @@ module.exports = function (app) {
    * I can start putting my routing middlewares here
    */
   app.get('/',(req, res, next)=>{
-    console.log('reqdze',req.body, req.fields, req.files)
     res.status(200)
       .format({
         html: _ => res.send('<p>It works!</p>'),
