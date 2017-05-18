66ab756b526dd38cd3c9043cbab6ff09df3d0b28
nash403 <nash403@outlook.fr>
2017-05-18 21:15:59 +0200

    add i18n file

---
+++ config/i18n.js
@@ -0,0 +1,5 @@
+/**
+ * Put here the i18n configuration of your choice.
+ * I recommend that you use the ~/locales directory to store your translation files.
+ * See https://www.npmjs.com/package/i18n by Marcus Spiegel
+ */
