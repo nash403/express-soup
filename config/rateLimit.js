4f49af35650e7e033be5d8c6a9ceeb0c1d137bac
nash403 <nash403@outlook.fr>
2017-05-18 20:08:47 +0200

    add simple rateLimit configuration

---
+++ config/rateLimit.js
@@ -0,0 +1,25 @@
+/**
+ * Basic rate-limiting setup.
+ * `require` this file wherever in your application to activate limitation.
+ *
+ * Note: (from https://www.npmjs.com/package/express-rate-limit)
+ * The `express-rate-limit` module does not share state with other
+ * processes/servers by default. If you need a more robust solution,
+ * I recommend adding the Redis Store or checking out strict-rate-limiter,
+ * express-brute, or rate-limiter.
+ * All are excellent pieces of software.
+ */
+module.exports = function rateLimit(app) { // pass express application object
+  let RateLimit = require('express-rate-limit');
+
+  let apiLimiter = new RateLimit({
+    windowMs: 15*60*1000, // 15 minutes
+    max: 100,
+    delayMs: 0 // disabled
+  });
+
+  //  apply to all requests
+  app.use(limiter);
+  // OR only apply to requests that begin for ex with /api/
+  // app.use('/api/', apiLimiter);
+}
