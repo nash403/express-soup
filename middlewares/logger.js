3d94327cbe3afff5a66bf518b216da8350b2b0cc
nash403 <nash403@outlook.fr>
2017-05-19 20:32:04 +0200

    extract env config for logrotation

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -5,36 +5,44 @@ module.exports = function logger(app) {
   const morgan = require('morgan');
   let logFormat = process.env.NODE_ENV === 'production' ? 'combined' : 'dev';
   // Uncomment following lines to enable log rotation
-  /*const fs = require('fs');
-  const rfs = require('rotating-file-stream');
-  let logDirectory = config.logDirectory;
+  if (process.env.LOGROTATION_ACTIVATE === 'true') {
 
-  // ensure log directory exists
-  fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+    const fs = require('fs');
+    const rfs = require('rotating-file-stream');
+    let logDirectory = config.logDirectory;
 
-  let generator = function generator(time, index) {
-    if(!time)
-        return 'access.log';
+    // ensure log directory exists
+    fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
 
-    function pad(num) {
-      return (num > 9 ? "" : "0") + num;
+    let generator = function generator(time, index) {
+      if(!time)
+          return `${process.env.LOGROTATION_FILENAME || 'access'}.log`;
+
+      function pad(num) {
+        return (num > 9 ? "" : "0") + num;
+      }
+
+      let day    = pad(time.getDate());
+      let date  = `${time.getFullYear()}-${pad(time.getMonth() + 1)}-${day}`;
+      let hour   = pad(time.getHours());
+      let minute = pad(time.getMinutes());
+
+      return `${process.env.LOGROTATION_FILENAME || 'access'}-${date}-${hour}hh${minute}mm.${index > 1 ? index +'.' : ''}log.gz`;
     }
 
-    let day    = pad(time.getDate());
-    let date  = `${time.getFullYear()}-${pad(time.getMonth() + 1)}-${day}`;
-    let hour   = pad(time.getHours());
-    let minute = pad(time.getMinutes());
+    // create a rotating write stream
+    let accessLogStream = rfs(generator, {
+      interval: process.env.LOGROTATION_INTERVAL,
+      size: process.env.LOGROTATION_SIZE,
+      compress: process.env.LOGROTATION_COMPRESSION,
+      path: logDirectory
+    })
+    app.use(morgan(logFormat , {stream: accessLogStream}))
 
-    return `access-${date}-${hour}hh${minute}mm.${index > 1 ? index +'.' : ''}log.gz`;
+  } else {
+
+    app.use(morgan(logFormat))
+
   }
 
-  // create a rotating write stream
-  let accessLogStream = rfs(generator, {
-    interval: '1d', // rotate daily
-    size:     '10M', // rotate every 10 MegaBytes written
-    compress: 'gzip', // compress rotated files
-    path: logDirectory
-  })*/
-
-  app.use(morgan(logFormat /*, {stream: accessLogStream}*/))
 }
